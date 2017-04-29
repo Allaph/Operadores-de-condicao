b03a3f87173855e7fb02a364d350ebdad403e35c
Absyd <allaph_anderson@hotmail.com>
2017-04-29 15:43:05 -0300

    operadores de condicao

---
+++ src/videoaula8/Videoaula8.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoaula8;
+
+/**
+ *
+ * @author absyd
+ */
+public class Videoaula8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // > indica maioridade (variavel 1 > variavel 2) (maior)
+        // < indica menoridade (variavel 1 < variavel 2) (menor)
+        //>= indica maioridade ou igualdade (variavel 1 >= variavel 2)(maior ou igual)
+        //<= indica inferioridade ou igualdade (variavel 1 <= variavel 2)(menor ou igual)
+        // == indica igualdade de valores (variavel 1 == variaveç 2) (igualdade)
+        
+//        int num1=2, num2=1;
+//        
+//        //sinal de maioridade
+//        if(num1> num2){
+//        System.out.println("Variavel 1 maior! ");
+//    } else{
+//            System.out.println("Variavel 1 menor! ");
+//            } 
+        //sinal de inferioridade
+        
+        int num1=3, num2=3;
+        
+//        if(num1< num2){
+//        System.out.println("Variavel 1 menor! ");
+//    } else{
+//            System.out.println("Variavel 1 maior! ");
+//            } 
+        
+        //sinal de maior ou igual
+        
+//        if(num1>= num2){
+//        System.out.println("Variavel 1 maior ou igual! ");
+//    } else{
+//            System.out.println("erro ");
+//            } 
+        
+        //igualdade
+        
+        if(num1== num2){
+        System.out.println("igual! ");
+    } else{
+            System.out.println("erro ");
+            } 
+        
+        
+    }
+    
+}
